898940b1367c7d852f43c51f31a7fc78cfebdd66
Xenia <andreeva19-91@mail.ru>
2024-01-13 04:59:54 +0300

    сделала стили для двух оставшихся величин, добавила слушатель событий на показать все/скрыть

---
+++ script.js
@@ -0,0 +1,32 @@
+let button = document.querySelector('.button');
+
+button.addEventListener ('click', function(evt) {
+    
+    evt.preventDefault();
+    
+    let slider = document.querySelector('.brand-list');
+
+    if (slider.classList.contains('brand-list--hidden')) {
+
+       slider.classList.remove('brand-list--hidden');
+       slider.classList.add('brand-list--unhidden'); 
+       
+       let buttonText = button.querySelector('.button__text');
+       buttonText.textContent = 'Скрыть';
+       
+       let buttonImg = button.querySelector('.button__img');
+       buttonImg.src = '/images/up.png';
+
+    } else if (slider.classList.contains('brand-list--unhidden')) {
+       
+       slider.classList.remove('brand-list--unhidden');
+       slider.classList.add('brand-list--hidden');
+       
+       let buttonText = button.querySelector('.button__text');
+       buttonText.textContent = 'Показать все';
+       
+       let buttonImg = button.querySelector('.button__img');
+       buttonImg.src = '/images/expand.png';
+
+    };
+});
