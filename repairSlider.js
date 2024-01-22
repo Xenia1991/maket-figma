3526c3c9c47bcb31c39621099cdb337776f00f4e
Xenia <andreeva19-91@mail.ru>
2024-01-22 13:51:37 +0700

    Сделала логику изменения размера экрана при нажатии кнопки, стили свайпер смогла перебить только костылем

--- repairSlider.js
+++ repairSlider.js
@@ -1,10 +1,14 @@
 let repairSwiper;
+let repairContainerElement = document.querySelector('.repair-list');
+let buttonElement = document.querySelector('.button');
 let screenWidth = document.querySelector('.page-container');
 
 if (screenWidth.clientWidth < 768) {
    
-    swiperPage = new Swiper('.swiper', {
+    repairSwiper = new Swiper('.swiper', {
        
+       spacebetween: '16',
+
        slidesPerView: '1.13',
        
        pagination: {
@@ -33,4 +37,33 @@ if (screenWidth.clientWidth < 768) {
 
  }
 
+let openRepairMenu = function () {
+
+    if (repairContainerElement.classList.contains('repair-list--hidden')) {
+
+        repairContainerElement.classList.remove('repair-list--hidden');
+        repairContainerElement.classList.add('repair-list--unhidden');
+
+        let buttonText = buttonElement.querySelector('.button__text');
+        buttonText.textContent = 'Скрыть';
+       
+        let buttonImg = buttonElement.querySelector('.button__img');
+        buttonImg.src = '/images/up.png';
+
+    } else if (repairContainerElement.classList.contains('repair-list--unhidden')) {
+
+        repairContainerElement.classList.remove('repair-list--unhidden');
+        repairContainerElement.classList.add('repair-list--hidden');
+        
+        let buttonText = buttonElement.querySelector('.button__text');
+        buttonText.textContent = 'Показать все';
+       
+        let buttonImg = buttonElement.querySelector('.button__img');
+        buttonImg.src = '/images/expand.png';
+
+    }
+
+}
+
  window.addEventListener ('resize', resizeWindow);
+ buttonElement.addEventListener('click', openRepairMenu);
